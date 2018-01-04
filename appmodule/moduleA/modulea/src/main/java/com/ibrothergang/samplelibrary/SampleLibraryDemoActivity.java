e5a34db0b56f8e61858492385af2d8cb2ccfa8e0
Vincent <gang.wang@quvideo.com>
2018-01-04 18:05:53 +0800

    add files

---
+++ appmodule/moduleA/modulea/src/main/java/com/ibrothergang/samplelibrary/SampleLibraryDemoActivity.java
@@ -0,0 +1,38 @@
+package com.ibrothergang.samplelibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class SampleLibraryDemoActivity extends AppCompatActivity {
+    private TextView mTxtInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_library_demo);
+        mTxtInfo = findViewById(R.id.text_info);
+    }
+
+    public void button01Click(View view) {
+        showText("button01Click");
+    }
+
+    public void button02Click(View view) {
+        showText("button02Click");
+    }
+
+    public void button03Click(View view) {
+        showText("button03Click");
+    }
+
+    public void ClearClick(View view) {
+        mTxtInfo.setText("");
+    }
+
+    private void showText(String value) {
+        CharSequence oldString = mTxtInfo.getText();
+        mTxtInfo.setText(oldString + "\n" + value);
+    }
+}
